5049a68c2465b13557b86ef427aeb90add87b812
jonahmeng <123894986+jonahmeng@users.noreply.github.com>
2023-02-12 22:34:53 +0000

    Highway

--- DSM_Python1/02-09-2023/highway.py
+++ DSM_Python1/02-09-2023/highway.py
@@ -1,3 +1,2 @@
 highwaynumber = int(input('Enter a valid 3-digit highway number: '));
 interstatehighway = int(input('Highway services primary interstate highway: '))
-print('thanks for typing your highway number')
