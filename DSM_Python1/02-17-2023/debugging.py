fe0ed320451488e57b5dba9c0c16fa4e87e8a505
jonahmeng <123894986+jonahmeng@users.noreply.github.com>
2023-02-18 01:32:59 +0000

    initial comment

---
+++ DSM_Python1/02-17-2023/debugging.py
@@ -0,0 +1,5 @@
+num_beans = int(input('How many beans? '))
+num_jars = int(input('How many jars? '))
+total_beans = int(input('How many total beans? '))
+total_beans = num_beans * num_jars
+print('total_beans')
